9e0138bf99c2b5d93a18e84d9af59bdb191c08e1
Peter Westman <pwestman@gmail.com>
2020-03-31 15:25:27 +0200

    stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'answer', to: 'questions#answer'
 end
